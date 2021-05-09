0aeda86f0ef33f6b99af0fc53c3e84f039ba1124
shanole <73411129+shanole@users.noreply.github.com>
2021-05-08 22:19:33 -0700

    add route to post new vendor to index

--- PierresBakeryWebsite/Controllers/VendorsController.cs
+++ PierresBakeryWebsite/Controllers/VendorsController.cs
@@ -18,5 +18,11 @@ namespace PierresBakeryWebsite.Controllers
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
     }
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
   }
 }
