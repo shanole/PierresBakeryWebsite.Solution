25f0826dd8dc060221fa0b5ec1421cb94049e06d
shanole <73411129+shanole@users.noreply.github.com>
2021-05-08 23:02:22 -0700

    added route for showing order details

--- PierresBakeryWebsite/Controllers/OrdersController.cs
+++ PierresBakeryWebsite/Controllers/OrdersController.cs
@@ -12,5 +12,15 @@ namespace PierresBakeryWebsite.Models
       Vendor selectedVendor = Vendor.Find(vendorId);
       return View(selectedVendor);
     }
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order selectedOrder = Order.Find(orderId);
+      Vendor selectedVendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("vendor",selectedVendor);
+      model.Add("order",selectedOrder);
+      return View("Show",model);
+    }
   }
 }
