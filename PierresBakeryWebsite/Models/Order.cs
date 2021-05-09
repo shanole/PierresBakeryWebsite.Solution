6f4264df86a8d74dfded8d52971c7852d71fa094
shanole <73411129+shanole@users.noreply.github.com>
2021-05-08 20:48:29 -0700

    add GetAll method

--- PierresBakeryWebsite/Models/Order.cs
+++ PierresBakeryWebsite/Models/Order.cs
@@ -22,6 +22,10 @@ namespace PierresBakeryWebsite.Models
    {
      _instances.Clear();
    }
+   public static List<Order> GetAll()
+   {
+     return _instances;
+   }
    public static Order Find(int searchId)
    {
      return _instances[searchId-1];
