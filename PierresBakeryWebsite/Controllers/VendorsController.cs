6d2d78256aec64f5309df3319e7b520252db35ac
shanole <73411129+shanole@users.noreply.github.com>
2021-05-08 22:34:41 -0700

    add route to show vendor details page

--- PierresBakeryWebsite/Controllers/VendorsController.cs
+++ PierresBakeryWebsite/Controllers/VendorsController.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System;
 using Microsoft.AspNetCore.Mvc;
 using PierresBakeryWebsite.Models;
 
@@ -23,6 +22,16 @@ namespace PierresBakeryWebsite.Controllers
     {
       Vendor newVendor = new Vendor(vendorName, vendorDescription);
       return RedirectToAction("Index");
+    }
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string,object> model = new Dictionary<string,object> {};
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor",selectedVendor);
+      model.Add("orders",vendorOrders);
+      return View(model);
     }
   }
 }
