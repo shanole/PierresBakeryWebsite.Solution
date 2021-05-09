d4b949f26413e23c87a3c8338011b148d7a5a03b
shanole <73411129+shanole@users.noreply.github.com>
2021-05-08 20:19:43 -0700

    pass test 3 for Order class

--- PierresBakeryWebsite.Tests/ModelTests/OrderTests.cs
+++ PierresBakeryWebsite.Tests/ModelTests/OrderTests.cs
@@ -11,8 +11,8 @@ namespace PierresBakeryWebsite.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceofOrder_Order()
     {
-      Order newOrder = new Order("One dozen loaves of bread",24,"April 21, 2021");
-      Assert.AreEqual(typeof(Order),newOrder.GetType());
+      Order newOrder = new Order("One dozen loaves of bread", 24, "April 21, 2021");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
     [TestMethod]
     public void GetDescription_ReturnsOrderDescription_String()
@@ -20,8 +20,17 @@ namespace PierresBakeryWebsite.Tests
       string description = "12 cookies";
       int price = 10;
       string date = "January 1, 2021";
-      Order newOrder = new Order(description,price,date);
+      Order newOrder = new Order(description, price, date);
       Assert.AreEqual(description, newOrder.Description);
+    }
+    [TestMethod]
+    public void GetPrice_ReturnsOrderDescription_String()
+    {
+      string description = "12 cookies";
+      int price = 10;
+      string date = "January 1, 2021";
+      Order newOrder = new Order(description, price, date);
+      Assert.AreEqual(price, newOrder.Price);
     }
   }
 }
